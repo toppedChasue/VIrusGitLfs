71205273f2b3b8c50a56cd54915dc53a479eeacc
toppedChasue <104607816+toppedChasue@users.noreply.github.com>
2022-08-15 20:55:04 +0900

    1

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Skill : MonoBehaviour
 {//일단 스킬은 스탑
     protected string skillName;
-    protected float skillDmg;
+    protected int skillDmg;
     protected float coolTime;
     protected float currentTime;
     protected float range;
 
+    protected bool isUnlock;
+
+    protected List<Skill> skillList;
+    protected List<Skill> unLockSkillList;
+
+    public Transform target;
+
+    Player player;
+
+    private void Start()
+    {
+        skillList = new List<Skill>();
+        unLockSkillList = new List<Skill>();
+        player = GetComponent<Player>();
+    }
+
     protected virtual void Init()
     {
-
+        target = player.target;
     }
 }
